df8e19740115407c26f6b27fc92c15e8582eec35
Ivan Semenov <ivanse@metropolia.fi>
2024-02-13 23:34:23 +0200

    Added tailwind + Discover.tsx + etc.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MusicGetAllContextProvider } from "./context/MusicGetAllContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MusicGetAllContextProvider>
+      <App />
+    </MusicGetAllContextProvider>
   </React.StrictMode>
 );
--- frontend/src/routes/PathConstants.tsx
+++ frontend/src/routes/PathConstants.tsx
@@ -2,6 +2,7 @@ const PathConstants = {
     HOME: "/",
     REGISTER: "/register",
     LOGIN: "/login",
+    DISCOVER: "/discover",
   };
   
   export default PathConstants;
