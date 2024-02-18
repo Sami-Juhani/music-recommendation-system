43a1d57e3e16ae11f5479c806634d8fadc243aec
Sami-Juhani <sami.paananen@gmail.com>
2024-02-18 15:40:13 +0200

    (feature) Adds ability to rate songs

--- backend/music_recommender/urls.py
+++ backend/music_recommender/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     path('api/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/recommendations/', include('recommendations.urls')),
+    path('api/song-ratings/', include('song_ratings.urls')),
 ]
