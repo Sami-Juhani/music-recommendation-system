0bfb04f4aa823ddca7b98707dcefe87820f72feb
Sami-Juhani <sami.paananen@gmail.com>
2024-02-23 15:55:35 +0200

    remove duplicate import

--- frontend/src/pages/LoginForm.tsx
+++ frontend/src/pages/LoginForm.tsx
@@ -5,9 +5,6 @@ import { Link } from "react-router-dom";
 import PathConstants from "../routes/PathConstants";
 import { useLogin } from '../hooks/useLogin'
 import '../styles/LoginForm.css';
-import useLogin from "../hooks/useLogin";
-
-
 
 const LoginForm = () => {
   const { formData, handleChange, handleSubmit } = useLogin();
