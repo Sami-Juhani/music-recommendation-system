115ec4844aa8108e630f7b18de9a86eab671318c
Sami-Juhani <sami.paananen@gmail.com>
2024-03-10 00:17:47 +0200

    Add basename for deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,37 @@
 import React from "react";
 import "./style.css";
-import { createBrowserRouter, RouterProvider  } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Page404 from "./pages/Page404";
 import PathConstants from "./routes/PathConstants";
-import {RegistrationRoute} from "./pages/RegistrationPage";
+import { RegistrationRoute } from "./pages/RegistrationPage";
 import ProfileUpdate from "./pages/ProfileUpdate";
 import { mainPageRoute } from "./pages/MainPage";
 
-
 function App() {
+  let BASE_NAME;
+  if (process.env.NODE_ENV === "production") {
+    BASE_NAME = "/music-recommender";
+  } else {
+    BASE_NAME = "/";
+  }
 
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        element: <Layout />,
+        children: [
+          { index: true, ...mainPageRoute },
+          { path: "*", element: <Page404 /> },
+          { ...RegistrationRoute },
+          { path: PathConstants.PROFILE_UPDATE, element: <ProfileUpdate /> },
+        ],
+      },
+    ],
     {
-      element: <Layout />,
-      children: [
-        { index: true, ...mainPageRoute },
-        { path: "*", element: <Page404 /> },
-        { ...RegistrationRoute },
-        { path: PathConstants.PROFILE_UPDATE, element: <ProfileUpdate /> },
-      ],
+      basename: BASE_NAME,
     }
-  ]);
- 
+  );
 
   return (
     <div className="relative flex">
