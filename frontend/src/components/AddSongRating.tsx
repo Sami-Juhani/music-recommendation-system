7fe32dfecfee1c876fa0324a7a10148575ec8b0c
baldez300 <baldez111990@gmail.com>
2024-03-10 13:04:20 +0200

    To prevent users from entering a rating value greater than 5

--- frontend/src/components/AddSongRating.tsx
+++ frontend/src/components/AddSongRating.tsx
@@ -7,8 +7,16 @@ const AddSongRating = ({ spotifyId }: { spotifyId: string }) => {
     const [rating, setRating] = useState(0);
 
     const handleRatingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setRating(Number(e.target.value));
-    };
+        let newRating = parseInt(e.target.value);
+
+        if (newRating < 1) {
+            newRating = 1;
+        } else if (newRating > 5) {
+            newRating = 5;
+        }
+
+        setRating(newRating);
+        };
 
     const handleSubmitRating = async () => {
         try {
