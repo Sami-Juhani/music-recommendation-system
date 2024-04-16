e244a9408369b0ab08acacea3717a9e96cf4b268
Sami-Juhani <sami.paananen@gmail.com>
2024-04-16 22:57:04 +0300

    remove old player and add play button for a track

--- frontend/src/components/PlayListTracks.tsx
+++ frontend/src/components/PlayListTracks.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useRef, useEffect } from "react";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import testAlbum from "../assets/album.webp";
+import { usePlayer } from "../context/usePlayer";
+import { Playlist } from "../types/PlayListInterface";
 import msToMinutesAndSeconds from "../utils/timeConvret";
 import PlayListHeader from "./PlayListHeader";
-import testAlbum from "../assets/album.webp";
-import { Playlist } from "../types/PlayListInterface";
 import SongRating from "./SongRating";
 
 interface TracksPlayListProps {
@@ -12,34 +13,13 @@ interface TracksPlayListProps {
   onePLIsLoading: boolean;
 }
 
-const TracksPlayList: React.FC<TracksPlayListProps> = ({
-  playlist,
-  onePLIsLoading,
-}) => {
+const TracksPlayList: React.FC<TracksPlayListProps> = ({ playlist, onePLIsLoading }) => {
   const [selectedSong, setSelectedSong] = useState<any>(null);
-  const audioRef = useRef<HTMLAudioElement>(null);
-
-  useEffect(() => {
-    const handleEnd = () => {
-      setSelectedSong({ ...selectedSong, isPlaying: false });
-    };
-
-    if (audioRef.current == null) return;
-
-    const currentAudioRef = audioRef.current;
-
-    currentAudioRef.addEventListener("ended", handleEnd);
-
-    return () => currentAudioRef.removeEventListener("ended", handleEnd);
-  }, [selectedSong]);
+  const { currentSong, playSong, setCurrentSong } = usePlayer();
 
   const handleSongClick = (song: any, e: React.MouseEvent<HTMLDivElement>) => {
-    const isRatingInput = (e.target as HTMLElement).classList.contains(
-      "rating-input"
-    );
-    const isSubmitButton = (e.target as HTMLElement).classList.contains(
-      "submit-button"
-    );
+    const isRatingInput = (e.target as HTMLElement).classList.contains("rating-input");
+    const isSubmitButton = (e.target as HTMLElement).classList.contains("submit-button");
 
     if (isRatingInput || isSubmitButton) {
       return;
@@ -52,29 +32,6 @@ const TracksPlayList: React.FC<TracksPlayListProps> = ({
     }
   };
 
-  const handleAudioToggle = (
-    song: any,
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    // Prevent the event from propagating to the parent elements
-    e.stopPropagation();
-
-    if (audioRef.current == null) return;
-
-    if (selectedSong && selectedSong.id === song.id) {
-      // Toggle the playing state for the current song
-      setSelectedSong({ ...selectedSong, isPlaying: !selectedSong.isPlaying });
-
-      if (selectedSong.isPlaying) audioRef.current.pause();
-      else audioRef.current.play();
-    } else {
-      // Select the new song and set its playing state to true
-      setSelectedSong({ ...song, isPlaying: true });
-      audioRef.current.src = song.preview_url;
-      audioRef.current.play();
-    }
-  };
-
   return (
     <div className="list">
       <PlayListHeader />
@@ -83,31 +40,38 @@ const TracksPlayList: React.FC<TracksPlayListProps> = ({
           playlist.tracks.items.map((item: any, index: number) => {
             return (
               <div
-                className="row"
-                key={index}
+                className={`row ${currentSong?.spotifyId === item.track.id ? "playing" : ""}`}
+                key={item.track.id}
                 onClick={(e) => handleSongClick(item.track, e)}
               >
                 <div className="col">
                   <span>{index + 1}</span>
+                  <FontAwesomeIcon
+                    icon={faPlayCircle}
+                    className="tracks-play-button"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      if (currentSong === undefined) return;
+                      playSong(item.track.id);
+                      setCurrentSong({
+                        name: item.track?.name,
+                        artist: item.track?.artists[0]?.name,
+                        progressMs: 0,
+                        duration: item.track.duration_ms,
+                        imageUrl: item.track.album.images[0].url,
+                        isPlaying: true,
+                        spotifyId: item.track.id,
+                      });
+                    }}
+                  />
                 </div>
                 <div className="col detail">
                   <div className="image">
-                    <img
-                      src={
-                        item.track.album.images
-                          ? item.track.album.images[0].url
-                          : testAlbum
-                      }
-                      alt="track"
-                    />
+                    <img src={item.track.album.images ? item.track.album.images[0].url : testAlbum} alt="track" />
                   </div>
                   <div className="info">
                     <span className="name">{item.track.name}</span>
-                    <span>
-                      {item.track.artists
-                        ? item.track.artists[0].name
-                        : "Artists unknown"}
-                    </span>
+                    <span>{item.track.artists ? item.track.artists[0].name : "Artists unknown"}</span>
                   </div>
                 </div>
                 <div className="col">
@@ -115,16 +79,6 @@ const TracksPlayList: React.FC<TracksPlayListProps> = ({
                 </div>
                 <div className="col">
                   <span>{msToMinutesAndSeconds(item.track.duration_ms)}</span>
-                </div>
-                <div className="col">
-                  {/* Display play/pause button */}
-                  {selectedSong && selectedSong.id === item.track.id ? (
-                    <button onClick={(e) => handleAudioToggle(item.track, e)}>
-                      <FontAwesomeIcon
-                        icon={selectedSong.isPlaying ? faPause : faPlay}
-                      />
-                    </button>
-                  ) : null}
                 </div>
                 <div className="col">
                   {selectedSong && selectedSong.id === item.track.id && (
@@ -142,14 +96,6 @@ const TracksPlayList: React.FC<TracksPlayListProps> = ({
           </>
         )}
       </div>
-      {/* Audio element for playback */}
-      {selectedSong && (
-        <audio
-          ref={audioRef}
-          src={selectedSong.preview_url}
-          autoPlay={selectedSong.isPlaying}
-        />
-      )}
     </div>
   );
 };
