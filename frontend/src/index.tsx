2d74335526bd5cfcc0a647f2ca3038c6dbe4819e
Sami-Juhani <sami.paananen@gmail.com>
2024-04-16 22:58:36 +0300

    add PlayerContextProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,30 +8,29 @@ import { PlaylistsGetAllContextProvider } from "./context/PlaylistsGetAllContext
 import { PlaylistGetContextProvider } from "./context/PlaylistGetContext";
 import { GeneratedContextProvider } from "./context/GeneratedContext";
 import { NotificationContextProvider } from "./context/NotificationContextProvider";
+import { PlayerContextProvider } from "./context/PlayerContextProvider";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import "./i18n"
+import "./i18n";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <NotificationContextProvider>
-      <UserContextProvider>
-        <MusicGetAllContextProvider>
-          <PlaylistsGetAllContextProvider>
-            <PlaylistGetContextProvider>
-              <GeneratedContextProvider>
-                <div className="relative flex">
+      <MusicGetAllContextProvider>
+        <PlaylistsGetAllContextProvider>
+          <PlaylistGetContextProvider>
+            <PlayerContextProvider>
+              <UserContextProvider>
+                <GeneratedContextProvider>
                   <RouterProvider router={router} />
-                </div>
-              </GeneratedContextProvider>
-            </PlaylistGetContextProvider>
-          </PlaylistsGetAllContextProvider>
-        </MusicGetAllContextProvider>
-      </UserContextProvider>
+                </GeneratedContextProvider>
+              </UserContextProvider>
+            </PlayerContextProvider>
+          </PlaylistGetContextProvider>
+        </PlaylistsGetAllContextProvider>
+      </MusicGetAllContextProvider>
     </NotificationContextProvider>
   </React.StrictMode>
 );
