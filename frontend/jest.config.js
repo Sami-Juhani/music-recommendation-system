a124c598c8d452f92a7e8f36e761ef755e464fad
Sami-Juhani <sami.paananen@gmail.com>
2024-04-24 21:12:54 +0300

    fix typo

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 ﻿// jest.config.js
 module.exports = {
     testEnvironment: 'jsdom',
-    roots: ['<rootDir>/src/__tests__/junit/jest'],
+    roots: ['<rootDir>/src/__tests__/unit/jest'],
     transform: {
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
